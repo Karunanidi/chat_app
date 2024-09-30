25aa2b5357f51a4a4aea34d4d7be988ac4989f5f
trisna.na <trisna.na@gmail.com>
2024-09-30 21:45:07 +0700

    update gradle

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white70),
         useMaterial3: true,
       ),
-      home: const AuthScreen(),
+      home: const AuthScreen()
     );
   }
 }
